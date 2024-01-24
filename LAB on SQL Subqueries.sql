118fb6cc20d8b9a0a434943b230ddc908e2da49d
Olivia Ojinji-Kotschka <155965632+Oliviasteph5@users.noreply.github.com>
2024-01-24 15:47:18 +0100

    LAB on SQL Subqueries
    Week 3 LAB on SQL Subqueries

---
+++ LAB on SQL Subqueries.sql
@@ -0,0 +1,29 @@
+
+USE sakila; 
+
+#Write SQL queries to perform the following tasks using the Sakila database:
+
+#1. Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+
+SELECT COUNT(inventory_id) AS num_of_copies
+FROM inventory
+WHERE film_id IN (SELECT film_id
+						 FROM film
+						 WHERE title = "Hunchback Impossible");
+
+#2. List all films whose length is longer than the average length of all the films in the Sakila database.
+SELECT title, length
+FROM film
+WHERE length > (SELECT AVG(length)
+                FROM film
+    );
+      
+#3. Use a subquery to display all actors who appear in the film "Alone Trip".
+SELECT first_name, last_name, actor_id
+FROM actor
+WHERE actor_id IN (SELECT actor_id
+					FROM film_actor
+                    WHERE film_id IN (SELECT film_id
+										FROM film
+										WHERE title = "Alone Trip"));
+                                                                
